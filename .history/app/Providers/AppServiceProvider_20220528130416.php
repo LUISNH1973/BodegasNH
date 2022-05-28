2164f0f271fb79ecccae25690cdd89a86da6e2cd
LUISNH1973 <romafe.sll@hotmail.com>
2022-05-28 14:49:24 +0200

    Añadimos archivo Procfile

---
+++ .history/app/Providers/AppServiceProvider_20220528130416.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\UrlGenerator;
+
+class AppServiceProvider extends ServiceProvider
+{
+    public function boot(UrlGenerator $url)
+    {
+        if (env('REDIRECT_HTTPS')) {
+       $url->formatScheme('https://');
+        }
+    }
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}

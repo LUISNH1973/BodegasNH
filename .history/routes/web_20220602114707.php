7c68acb3210358ca493c234bf8ec4770b0c40b4d
LUISNH1973 <romafe.sll@hotmail.com>
2022-06-02 11:49:28 +0200

    rouetes get

--- .history/routes/web_20220602114707.php
+++ .history/routes/web_20220602114707.php
@@ -14,7 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [App\Http\Controllers\PortafolioController::class, 'index']);
-Route::get('/proyectos', [App\Http\Controllers\ProyectosController::class, 'form'])->name('proyectos');
 
 Auth::routes();
 
@@ -22,4 +21,4 @@ Route::resource('/proyectos', App\Http\Controllers\ProyectoController::class);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-
+Route::get('/proyectos', [App\Http\Controllers\ProyectosController::class, 'form'])->name('proyectos');
